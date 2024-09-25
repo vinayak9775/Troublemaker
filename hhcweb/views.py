9de485827482c569078c67078c0a69f024e69546
DESKTOP-50CAJ5Q\admin <waghvinayak16@gmail.com>
2024-09-25 18:01:50 +0530

    tesst

--- hhcweb/views.py
+++ hhcweb/views.py
@@ -114,6 +114,12 @@ def whatsapp_sms(to_number,template_name,placeholders):
     conn.close()
     print('hii')
     print('hii22')
+    print('hii')
+    print('hii22')
+    print('hii')
+    print('hii22')
+    print('hii')
+    print('hii22')
    
     if res.status == 200:
         return Response({"status": "Message sent successfully", "response": json.loads(data.decode("utf-8"))})
