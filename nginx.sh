861276c1199ffbc69e10cd596687c121cca3adff
DESKTOP-50CAJ5Q\admin <waghvinayak16@gmail.com>
2024-09-25 10:14:39 +0530

    added 777 perminssion

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo cp -rf HHC_cicd.conf /etc/nginx/sites-available/HHC_cicd
-chmod 710 /var/lib/jenkins/workspace/HHC_CICD
+chmod 777 /var/lib/jenkins/workspace/HHC_CICD
 
 sudo ln -s /etc/nginx/sites-available/HHC_cicd /etc/nginx/sites-enabled
 sudo nginx -t
