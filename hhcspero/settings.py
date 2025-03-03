5cac08d0ecc3ca8fe2662ad526420d0028008acb
DESKTOP-50CAJ5Q\admin <waghvinayak16@gmail.com>
2025-03-03 17:34:40 +0530

    added db to project

--- hhcspero/settings.py
+++ hhcspero/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'hhcspero.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
+    # }
 
     # 'default': {
     #     'ENGINE' : 'django.db.backends.postgresql',
@@ -106,17 +106,17 @@ DATABASES = {
     # }
 
     
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'test_cicd',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'postgres',
-    #     'HOST': '192.168.1.109',
-    #     'PORT': '5432',  # Default PostgreSQL port
-    #     'OPTIONS': {
-    #         'options': '-c timezone=Asia/Kolkata',
-    #     },
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_cicd',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '192.168.1.109',
+        'PORT': '5432',  # Default PostgreSQL port
+        'OPTIONS': {
+            'options': '-c timezone=Asia/Kolkata',
+        },
+    }
 }
 
 
