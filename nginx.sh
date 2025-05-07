6ee7e9a330060f0177b4551e94deef3d15bc5535
DESKTOP-50CAJ5Q\admin <waghvinayak16@gmail.com>
2025-05-07 18:01:47 +0530

    test

--- nginx.sh
+++ nginx.sh
@@ -5,7 +5,7 @@ chmod 777 /var/lib/jenkins/workspace/HHC_CICD
 
 sudo ln -s /etc/nginx/sites-available/HHC_cicd /etc/nginx/sites-enabled
 sudo nginx -t
-
+ 
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
